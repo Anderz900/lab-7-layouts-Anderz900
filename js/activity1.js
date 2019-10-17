f9cbac94975e56e713bb7440964256e701e45f32
Andres Rivera <andres.r.rivera97@gmail.com>
2019-10-17 00:32:08 -0400

    fixed pesky syntax error; final version

--- js/activity1.js
+++ js/activity1.js
@@ -9,18 +9,54 @@ let svg = d3.select("#chart-area").append("svg")
 
 // 1) INITIALIZE FORCE-LAYOUT
 
-
 // Load data
 d3.json("data/airports.json")
 .then(function(data) {
-  
+  let force = d3.forceSimulation(data.nodes)
+  .force("charge", d3.forceManyBody().strength(-10))
+  .force("link", d3.forceLink(data.links).distance(30))
+  .force("center", d3.forceCenter().x(width/2).y(height/2));
   // 2a) DEFINE 'NODES' AND 'EDGES'
+
+  let node = svg.selectAll(".node")
+        .data(data.nodes)
+  	    .enter().append("circle")
+    		.attr("class", "node")
+    		.attr("r", 5)
+        .attr("fill", "green");
+        
+  let edges = svg.selectAll("line")
+        .data(data.edges)
+        .enter().append("line")
+        .style("stroke", "#ccc")
+        .style("stroke-width", 1);
   // 2b) START RUNNING THE SIMULATION
 
   // 3) DRAW THE LINKS (SVG LINE)
 
   // 4) DRAW THE NODES (SVG CIRCLE)
 
+    node.call(d3.drag()
+         .on("start", dragStarted)
+         .on("drag", dragging)
+         .on("end", dragEnded));
   // 5) LISTEN TO THE 'TICK' EVENT AND UPDATE THE X/Y COORDINATES FOR ALL ELEMENTS
 
+  force.on("tick", function(data) {
+		
+    // Update node coordinates
+    
+        node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+    
+        node.append("title")
+        .text(function(d) { return d.name; });
+       
+    
+    // Update edge coordinates
+    edges.attr("x1", function(d) { return d.source.x; })
+    .attr("y1", function(d) { return d.source.y; })
+    .attr("x2", function(d) { return d.target.x; })
+    .attr("y2", function(d) { return d.target.y; }); 
+});
 });
--- js/activity2.js
+++ js/activity2.js
@@ -2,38 +2,88 @@
 let width = 1000,
 height = 600;
 
+
+
 let svg = d3.select("#chart-area").append("svg")
 .attr("width", width)
 .attr("height", height);
 
+d3.select('body').style('background', '#abcdef')
+
 // Load data
 
 // Load shapes of U.S. counties (TopoJSON)
-d3.json("data/us-10m.json")
+Promise.all([d3.json('./data/airports.json'),d3.json('./data/world-110m.json')])
 .then(function(data) {
-    let projection = d3.geoAlbersUsa()
-    .translate([width / 2, height / 2]);
 
-    let path = d3.geoPath()
-    .projection(projection);
+	
+let projection = d3.geoMercator()
+	.translate([width/2, height/2])
+	.center([0,0]);
+
+let path = d3.geoPath().projection(projection);
+
+let airportData = data[0];
+let globeData = data[1];
+
+
+
 
 
 	// Convert TopoJSON to GeoJSON (target object = 'states')
-	let usa = topojson.feature(data, data.objects.states).features
+let globe = topojson.feature(globeData, globeData.objects.countries).features
 	
 	// Render the U.S. by using the path generator
-	svg.selectAll("path")
-			.data(usa)
-		.enter().append("path")
-			.attr("d", path);
+svg.selectAll("path")
+	.data(globe)
+	.enter().append("path")
+	.attr("d", path)
+	.attr("fill", "#5548ff");
+
+
+let node = svg.selectAll('.node')
+	.data(airportData.nodes)
+	.enter().append('circle')
+	.attr('r', 5)
+	.attr('class', 'node')
+	.attr('fill', (d)=>{
+		if(d.country === "United States"){return 'red'}
+		else{ return 'purple'}})
+	.attr('cy', function(d){return d.y})
+	.attr('cx', function(d){return d.x})
+	.attr("transform", function(d) {
+		return "translate(" + projection([d.longitude, d.latitude]) + ")";
+	})
+	
+node.on('hover', function(d){
+	d.append("title")
+	.text(d=>d.name)
+	})
+
+
+let an = airportData.nodes;
+
+let connect = svg.selectAll('connect')
+	.data(airportData.links)
+	.enter().append('line')
+	.attr('stroke', '#ddd')
+	.attr('class', 'connect')
+	.attr('x1', function(d){
+		let source = d.source
+		return projection([an[source].longitude, an[source].latitude])[0]
+	})
+	.attr('x2', function(d){
+		let target = d.target
+		return projection([an[target].longitude, an[target].latitude])[0]
+	})
+	.attr('y1', function(d){
+		let source = d.source
+		return projection([an[source].longitude, an[source].latitude])[1]
+	})
+	.attr('y2', function(d){
+		let target = d.target
+		return projection([an[target].longitude, an[target].latitude])[1]
+	})
 });
 
 
-// 2a) DEFINE 'NODES' AND 'EDGES'
-// 2b) START RUNNING THE SIMULATION
-
-// 3) DRAW THE LINKS (SVG LINE)
-
-// 4) DRAW THE NODES (SVG CIRCLE)
-
-// 5) LISTEN TO THE 'TICK' EVENT AND UPDATE THE X/Y COORDINATES FOR ALL ELEMENTS
